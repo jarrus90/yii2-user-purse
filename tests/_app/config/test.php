a2ccadcc24783d1626ddb8f77ffa4d7bef6c58d2
Vladimir Stovbchatij <unreality90@mail.ru>
2016-09-22 13:26:19 +0300

    Tests init

---
+++ tests/_app/config/test.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'id' => 'yii2-user-tests',
+    'basePath' => dirname(__DIR__),
+    'language' => 'en-US',
+    'aliases' => [
+        '@jarrus90/UserPurse' => dirname(dirname(dirname(__DIR__))),
+        '@tests' => dirname(dirname(__DIR__)),
+        '@vendor' => VENDOR_DIR,
+        '@bower' => VENDOR_DIR . '/bower-asset',
+    ],
+    'bootstrap' => [
+        'jarrus90\User\Bootstrap',
+        'jarrus90\UserPurse\Bootstrap'
+     ],
+    'modules' => [
+        'user' => [
+            'class' => 'jarrus90\User\Module'
+        ],
+        'user-purse' => [
+            'class' => 'jarrus90\UserPurse\Module'
+        ],
+    ],
+    'components' => [
+        'db' => require __DIR__ . '/db.php',
+        'mailer' => [
+            'useFileTransport' => true,
+        ],
+        'urlManager' => [
+            'showScriptName' => true,
+        ],
+        'request' => [
+            'cookieValidationKey' => 'test',
+            'enableCsrfValidation' => false,
+        ],
+    ],
+    'params' => [],
+];
